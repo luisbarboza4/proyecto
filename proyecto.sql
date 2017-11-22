915d020b5251fe0b54a823dc36969ca16f89c45b
luisbarboza4 <luisbarboza1990@gmail.com>
2017-11-22 19:26:35 -0300

    DB

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-11-2017 a las 01:24:07
+-- Tiempo de generación: 22-11-2017 a las 23:25:19
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 5.6.31
 
@@ -25,103 +25,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `articulos_carrito`
+-- Estructura de tabla para la tabla `soporte`
 --
 
-CREATE TABLE `articulos_carrito` (
-  `id_carrito` int(11) NOT NULL,
-  `id_img_tam` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `carrito`
---
-
-CREATE TABLE `carrito` (
+CREATE TABLE `soporte` (
   `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `config`
---
-
-CREATE TABLE `config` (
-  `name` varchar(100) NOT NULL,
-  `value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `imagenes`
---
-
-CREATE TABLE `imagenes` (
-  `id` int(11) NOT NULL,
-  `ruta` text NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `mostrar` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `imagen_tamaño`
---
-
-CREATE TABLE `imagen_tamaño` (
-  `id` int(11) NOT NULL,
-  `id_imagen` int(11) NOT NULL,
-  `id_tamaño` int(11) NOT NULL,
-  `costo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tamaño`
---
-
-CREATE TABLE `tamaño` (
-  `id` int(11) NOT NULL,
-  `tamaño` varchar(30) NOT NULL,
-  `resolucion` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `type` enum('Admin','User') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `votos`
---
-
-CREATE TABLE `votos` (
-  `id_imagen` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `voto` enum('GOOD','BAD') NOT NULL
+  `name` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -129,39 +38,9 @@ CREATE TABLE `votos` (
 --
 
 --
--- Indices de la tabla `carrito`
+-- Indices de la tabla `soporte`
 --
-ALTER TABLE `carrito`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `config`
---
-ALTER TABLE `config`
-  ADD PRIMARY KEY (`name`);
-
---
--- Indices de la tabla `imagenes`
---
-ALTER TABLE `imagenes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `imagen_tamaño`
---
-ALTER TABLE `imagen_tamaño`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tamaño`
---
-ALTER TABLE `tamaño`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `user`
---
-ALTER TABLE `user`
+ALTER TABLE `soporte`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -169,29 +48,9 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT de la tabla `carrito`
+-- AUTO_INCREMENT de la tabla `soporte`
 --
-ALTER TABLE `carrito`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `imagenes`
---
-ALTER TABLE `imagenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `imagen_tamaño`
---
-ALTER TABLE `imagen_tamaño`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `tamaño`
---
-ALTER TABLE `tamaño`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `user`
---
-ALTER TABLE `user`
+ALTER TABLE `soporte`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
