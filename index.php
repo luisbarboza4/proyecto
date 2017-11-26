ed1884abe73a2e520b55319f6b6c8e0c01edcce6
luisbarboza4 <luis@phanta-soft.com>
2017-11-26 13:34:02 -0400

    emancipado

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+  	<link rel="stylesheet" type="text/css" href="js/jquery-ui-1.12.1/jquery-ui.min.css">
+  	<link rel="stylesheet" type="text/css" href="css/loading.css">
+  	<link rel="stylesheet" type="text/css" href="css/styles.css">
+  	<link rel="stylesheet" type="text/css" href="css/sweetalert.css">
+  	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Saira+Semi+Condensed" rel="stylesheet">
+  	<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+  	<script type="text/javascript" src="js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+  	<script type="text/javascript" src="js/bootstrap.js"></script>
+  	<script type="text/javascript" src="js/sweetalert.js"></script>
+  	<script src="js/loading.js"></script>
+  	<meta charset="utf-8">
+  	<title>TSUKUYOMI STORE</title>
+  </head>
+  <body class="index">
+
+    <div class="row">
+
+      <div class="col-sm-6 col-sm-offset-3 text-center index-buttons">
+        <center>
+          <a href="presencia.php"><img class="entry" src="img/indexblue.png"></a>
+          <br>
+          <a href="catalogo.php"><img class="entry" src="img/indexpink.png"></a>
+          <br>
+        </center>
+        <h4>tsukuyomi</h4>
+      </div>
+
+    </div>
+
+  </body>
+</html>
