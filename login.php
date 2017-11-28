3613d51eb3394d973fc34b50a070cd2d12346511
luisbarboza4 <luis@phanta-soft.com>
2017-11-28 09:13:24 -0400

    cambios css login

--- login.php
+++ login.php
@@ -3,23 +3,19 @@
 <form action="control.php" method="post" name="login_form" class="">
 <div class="col-md-offset-2 col-md-8 col-md-offset-2 col-xs-8">
   <?php if($errormsg)showMessage($errormsg,'error'); ?>
-    <div class="row">
+    <div class="row" style="margin-top: 10%; text-align: center;">
       <div class="form-group">
-        <label for="usuario">Usuario</label>
+        <label for="usuario" style="margin-bottom: 2%">Usuario</label>
         <input type="text" class="form-control" name="usuario" id="usuario" maxlength="40" placeholder="Ingrese el usuario" value="" required>
       </div>
+      <br><br>
       <div class="form-group">
-        <label for="clave">Clave</label>
+        <label for="clave" style="margin-bottom: 2%">Clave</label>
         <input type="password" class="form-control" name="clave" id="clave" maxlength="20" placeholder="Ingrese la clave" value="" required>
       </div>
-      <?php 
-      if(isset($_GET['error'])){
-        //$msj = "Usuario y/o contraseña inválidos";
-        //echo msj($msj,'danger');
-      }
-      ?>
+      <br><br>
       <div class='form-group'>
-            <button type='submit' class='btn'>Entrar</button>
+            <button type='submit' class='btn elim'>Entrar</button>
       </div>
     </div>
   </div>
